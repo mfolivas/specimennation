7c1db15762671056d1a67a762c0f79d60b0ac52e
molivas <molivas@3cinteractive.com>
2013-11-08 17:36:31 -0500

    Added the blog blog details and shortcodes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,16 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def blog = Action {
+    Ok(views.html.blog("Your new application is ready."))
+  }
+
+  def blogdetails = Action {
+    Ok(views.html.blogdetails("Your new application is ready."))
+  }
+
+  def shortcodes = Action {
+    Ok(views.html.shortcodes("Your new application is ready."))
+  }
+
 }
